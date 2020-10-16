eddba1098af53e045e170ee98ba83d460c7c26c7
Vadim Colcev <vadimkolchev@gmail.com>
2020-10-16 21:48:46 +0300

    Refactored view controller code

--- Keymap changer/ViewController.swift
+++ Keymap changer/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    var layoutDict = ["q": "й", "w": "ц", "e": "у", "r": "к", "t": "e", "y": "н", "u": "г", "i": "ш", "o": "щ", "p": "з", "a": "ф", "s": "ы", "d": "в", "f": "а", "g": "п", "h": "р", "j": "о", "k": "л", "l": "д", "z": "я", "x": "ч", "c": "с", "v": "м", "b": "и", "n": "т", "m": "ь", "[": "х", "]": "ъ", "\\": "ё", ";": "ж", "'": "э", ",": "б", ".": "ю"]
+    var layoutDict = ["q": "й", "w": "ц", "e": "у", "r": "к", "t": "e", "y": "н", "u": "г", "i": "ш", "o": "щ", "p": "з", "a": "ф", "s": "ы", "d": "в", "f": "а", "g": "п", "h": "р", "j": "о", "k": "л", "l": "д", "z": "я", "x": "ч", "c": "с", "v": "м", "b": "и", "n": "т", "m": "ь", "[": "х", "]": "ъ", "\\": "ё", ";": "ж", "'": "э", ",": "б", ".": "ю", "й": "q", "ц": "w", "у": "e", "к": "r", "е": "t", "н": "y", "г": "u", "ш": "i", "щ": "o", "з": "p", "ф": "a", "ы": "s", "в": "d", "а": "f", "п": "g", "р": "h", "о": "j", "л": "k", "д": "l", "я": "z", "ч": "x", "с": "c", "м": "v", "и": "b", "т": "n", "ь": "m", "х": "[", "ъ": "]", "ё": "\\", "ж": ";", "э": "'", "б": ",", "ю": "."]
     
     @IBOutlet weak var inputField: UITextField!
 
@@ -26,19 +26,9 @@ class ViewController: UIViewController {
             let text:String = inputField.text!
             var newString = ""
             for char in text {
-                var charFound = false
-                for (key, value) in layoutDict {
-                    if String(char) == key {
-                        newString += String(value)
-                        charFound = true
-                        break
-                    } else if String(char) == value {
-                        newString += String(key)
-                        charFound = true
-                        break
-                    }
-                }
-                if charFound == false {
+                if let fragment = layoutDict[String(char)] {
+                    newString += fragment
+                } else {
                     newString += String(char)
                 }
             }
